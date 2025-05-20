d4ab0f36338de4ceded13a40925ad8899846c0bc
Naveen pandian <npandian515@gmail.com>
2025-05-20 19:03:35 +0530

    Refactor mood_journal.py to improve code organization and readability

--- ai_mood_journal/mood_journal.py
+++ ai_mood_journal/mood_journal.py
@@ -5,3 +5,18 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import os 
 
+# to start the speech recognition
+recognizer = sr.Recognizer();
+
+# to load the model that fine-tuned 
+emotion_classifier = pipeline("text-classification",
+                              model="bhadresh-savani/distilbert-base-uncased-emotion");
+
+# the file where all journals are stored
+JOURNAL_FILE = "mood_journal.txt";
+
+
+# now lets cook a recording voice procedure
+
+def record_voice():
+    
